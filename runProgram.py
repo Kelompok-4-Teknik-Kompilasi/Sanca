44bb06787cc7ccc4f6704959b5783cc80ab2e412
pitekiren <pitektiren@gmail.com>
2021-06-21 13:27:14 +0700

    Finishing

---
+++ runProgram.py
@@ -0,0 +1,25 @@
+from lexer import Lexer
+from parser_ import Parser
+from interpreter import Interpreter, SymbolTable, Context, global_symbol_table
+
+
+def run(fn, text):
+    # Generate tokens
+    lexer = Lexer(fn, text)
+    tokens, error = lexer.make_tokens()
+    if error:
+        return None, error
+
+    # Generate AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+    if ast.error:
+        return None, ast.error
+
+    # Run program
+    interpreter = Interpreter()
+    context = Context('<program>')
+    context.symbol_table = global_symbol_table
+    result = interpreter.visit(ast.node, context)
+
+    return result.value, result.error
---
+++ sanca.py
@@ -0,0 +1,14 @@
+import runProgram
+while True:
+    text = input('sanca > ')
+    if text.strip() == "":
+        continue
+    result, error = runProgram.run('<stdin>', text)
+
+    if error:
+        print(error.as_string())
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
